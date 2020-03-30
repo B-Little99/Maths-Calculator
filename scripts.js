2127143dceebdaddcf378a0aa75f7c25a0a3bafb
Brandon Little <brandonlittle1999@hotmail.com>
2020-03-30 12:08:11 +0100

    Added the HTML structure and started the CSS. Basic functionality is included in the JS.

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let answer; 
+let displayValue;
+
+function add(a, b) {
+    answer = a + b;
+    return answer;
+}
+
+function multiply (a, b) {
+    answer = a * b;
+    return answer;
+}
+
+function subtract (a, b) {
+    answer = a - b;
+    return answer;
+}
+
+function divide (a, b) {
+    answer = a / b;
+    return answer;
+}
+
+function operate () {
+
+}
