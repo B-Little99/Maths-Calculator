39bca8358ef4f6f366b189ffb20517842c9c6282
Brandon Little <brandonlittle1999@hotmail.com>
2020-04-01 17:54:54 +0100

    Added JS functionality and some HTML

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
 // let answer; 
 let storedValue;
-let currentDisplay = document.getElementById("current");
+
 let operators = document.getElementsByClassName("operator");
 let numbers = document.getElementsByClassName("number");
 let equalsButton = document.getElementById("equals");
 let allClearButtons = document.getElementById("allClear");
 let deleteButton = document.getElementById("delete");
-let numberHistory = document.getElementById("history");
 
+let decimalButton = document.getElementById("decimal").innerHTML;
+
+/* Display output */
+let historyDisplay = document.getElementById("history");
+let currentDisplay = document.getElementById("current");
+
+/* Buttons */
 let one = document.getElementById("btn-1").innerHTML;
 let two = document.getElementById("btn-2").innerHTML;
 let three = document.getElementById("btn-3").innerHTML;
@@ -18,6 +24,14 @@ let seven = document.getElementById("btn-7").innerHTML;
 let eight = document.getElementById("btn-8").innerHTML;
 let nine = document.getElementById("btn-9").innerHTML;
 let zero = document.getElementById("btn-0").innerHTML;
+
+/* Operators */
+let divideSign = document.getElementById("divide").innerHTML;
+let plus = document.getElementById("add").innerHTML;
+let minus = document.getElementById("subtract").innerHTML;
+let times = document.getElementById("multiply").innerHTML;
+let storedOperator;
+let storedDisplay;
 
 /* Clears the calculator screen when AC is clicked */
 allClearButtons.addEventListener("click", function clear(){
@@ -32,11 +46,72 @@ function delBtn() {
 
 /* This function allows the user to include a decimal place, but only one. */
 
-function decimalPlace() {
+function decimalPlace() { 
+    let n = currentDisplay.indexOf(".");
+    alert(n);
+    if (n == -1) {
+        currentDisplay.innerHTML += decimalButton.innerHTML;
+    return currentDisplay;
+    }
 
 }
 
-/* displays the button number that is clicked in the display bar for each number */
+/* Equals button code */
+
+function equalsOperation() {
+
+}
+
+operators.addEventListener("click", function determineOperator(n){
+    let newOperator = n.innerHTML;
+    switch(newOperator) {
+        case "x":
+            storedOperator = "*"
+            multiply(); 
+            break;
+        case "÷":
+            storedOperator = "/"
+            divide() 
+            break;
+        case "-":
+            storedOperator = "-"
+            subtract() 
+            break;
+        case "+":
+            storedOperator = "+"
+            add();
+            break;  
+    }
+})
+
+
+/* These functions display the operator icon */
+
+function displayAdd() {
+    storedOperator = plus;
+    historyDisplay.innerHTML = currentDisplay.innerHTML + plus;
+    currentDisplay.innerHTML = '';
+}
+
+function displayDivide() {
+    storedOperator = divideSign;
+    historyDisplay.innerHTML = currentDisplay.innerHTML + divi;
+    currentDisplay.innerHTML = '';
+}
+
+function displayMinus() {
+    storedOperator = minus;
+    historyDisplay.innerHTML = currentDisplay.innerHTML;
+    currentDisplay.innerHTML = '';
+}
+
+function displayMultiply() {
+    storedOperator = times;
+    historyDisplay.innerHTML = currentDisplay.innerHTML;
+    currentDisplay.innerHTML = '';
+}
+
+/* These functions display the button number that is clicked on in the display bar */
 function calc1() {
     currentDisplay.innerHTML += one;
     return currentDisplay;
@@ -88,22 +163,25 @@ function calc0() {
 }
 
 /* Operator functionality */
-function add(a, b) {
-    answer = a + b;
-    return answer; 
+function add() {
+    answer = historyDisplay + storedOperator + currentDisplay;
+    let solution = eval(answer); 
+    historyDisplay = answer;
+    currentDisplay = solution;
+
 }
 
-function multiply(a, b) {
+function multiply() {
     answer = a * b;
     return answer;
 }
 
-function subtract(a, b) {
+function subtract() {
     answer = a - b;
     return answer;
 }
 
-function divide(a, b) {
+function divide() {
     answer = a / b;
     return answer;
 }
